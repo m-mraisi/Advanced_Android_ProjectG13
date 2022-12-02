51755d2e96f268318fc2dd45bd6f1c3f3473a2d4
m-mraisi <mahmodmreasi@gmail.com>
2022-12-02 13:30:33 -0500

    Initial commit

---
+++ app/src/main/java/com/G13/group/fragments/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.G13.group.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.G13.group.R
+import com.G13.group.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    val TAG:String = "LOGIN-FRAGMENT"
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: android.view.View, savedInstanceState: android.os.Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
