21710489d4942e6e29a46b18c32a2389917c7515
m-mraisi <mahmodmreasi@gmail.com>
2022-12-04 10:55:20 -0500

    Added the Sign Up page, the custom classes, and the boarding page

--- app/src/main/java/com/G13/group/MainActivity.kt
+++ app/src/main/java/com/G13/group/MainActivity.kt
@@ -2,12 +2,27 @@ package com.G13.group
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // toolbar code
+        //TODO NavBar
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.loginFragment) as NavHostFragment
+//        this.navController = navHostFragment.navController
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    // new code test
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
---
+++ app/src/main/java/com/G13/group/models/Comment.kt
@@ -0,0 +1,7 @@
+package com.G13.group.models
+
+class Comment(var commentDetails:String, var username:String) {
+    override fun toString(): String {
+        return "Comment(commentDetails='$commentDetails', username='$username')"
+    }
+}
