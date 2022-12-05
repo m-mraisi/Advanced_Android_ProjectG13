a24f19ea670d7b8cca5cc8a2f2f2e0c5282ba60d
m-mraisi <mahmodmreasi@gmail.com>
2022-12-04 19:49:47 -0500

    completed the functionalities of the SignUpFragment.kt

--- app/src/main/java/com/G13/group/MainActivity.kt
+++ app/src/main/java/com/G13/group/MainActivity.kt
@@ -6,13 +6,17 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.G13.group.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // toolbar code
         //TODO NavBar
